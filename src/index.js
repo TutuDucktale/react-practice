f3815246603f5866d5030bb291b96c178ade59aa
TutuDucktale <tutuchando@ducktaleit.com>
2026-01-30 16:55:16 +0530

    add new files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Card from "./Cards";
+import "./App.css";
+import Sdata from "./Sdata";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function ncard(val) {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      seriesname={val.seriesname}
+      link={val.link}
+    />
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <h1 className="heading_style">List Of My Top 5 Netflix Series </h1>
+    {Sdata.map(ncard)}
+  </>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
